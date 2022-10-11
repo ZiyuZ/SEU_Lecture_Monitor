507c2e6aeee45505a67b0a6d19c54671321088b4
ZiyuZ <19648091+ZiyuZ@users.noreply.github.com>
2022-10-11 22:01:41 +0800

    :new: Implement repeated get the lectures
    Use command line arguments to fetch lectures repeatedly
    Fine tune emoji

--- src/call_browser.py
+++ src/call_browser.py
@@ -2,4 +2,10 @@ import webbrowser
 
 from config import Config as C
 
-webbrowser.open(C.URLs.lecture_page, new=0, autoraise=True)
+
+def call_browser():
+    webbrowser.open(C.URLs.lecture_page, new=0, autoraise=True)
+
+
+if __name__ == "__main__":
+    call_browser()
--- src/lecture.py
+++ src/lecture.py
@@ -1,23 +1,36 @@
+import argparse
+import time
+from typing import List, Tuple
+
 import requests
 from rich.console import Console
 from rich.table import Table
 
+from call_browser import call_browser
 from config import Config as C
 from login import login_ehall
 
 
-def fetch_lecture_list(session: requests.Session) -> list:
+def fetch_lecture_list(session: requests.Session) -> Tuple[List, int]:
     res = session.get(C.URLs.lecture_list)
     lectures = res.json()["datas"]
+    available_lectures = 0  # 可用讲座数量
     # 过滤多余内容
     for lecture in lectures:
         lecture["JZXL_DISPLAY"] = lecture["JZXL_DISPLAY"].replace("人文与科学素养系列讲座_", "")
-        lecture["SFKSYY"] = "❌" if lecture["SFKSYY"] == 0 else "✔"
+        lecture["HDZRS"] = int(lecture["HDZRS"])
+        if lecture["SFKSYY"] == 0:
+            lecture["SFKSYY"] = "🔒"  # "❌"
+        elif lecture["YYRS"] < lecture["HDZRS"]:
+            lecture["SFKSYY"] = "✔"
+            available_lectures += 1
+        else:
+            lecture["SFKSYY"] = "🈵"
         lecture["YYRS_HDZRS"] = f'{lecture["YYRS"]}/{lecture["HDZRS"]}'
-    return lectures
+    return lectures, available_lectures
 
 
-def render_table(lectures: list):
+def render_table(lectures: List):
     # 没有讲座时直接返回
     if len(lectures) == 0:
         C.logger.info("❌ No lecture found.")
@@ -38,9 +51,58 @@ def render_table(lectures: list):
     )
 
 
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        prog="SEU Lecture Monitor",
+        description="获取东南大学讲座列表.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "-r",
+        "--repeat",
+        dest="repeat",
+        action="store_true",
+        help="定时获取讲座列表. 当发现有可用讲座时, 启动浏览器.",
+    )
+    parser.add_argument(
+        "-i",
+        "--interval",
+        dest="interval",
+        type=int,
+        default=60,
+        nargs="?",
+        help="定时获取讲座的尝试时间间隔, 在使用参数 '-r' 后生效. 单位: 秒.",
+    )
+    parser.add_argument(
+        "-t",
+        "--times",
+        dest="times",
+        type=int,
+        default=3,
+        nargs="?",
+        help="定时获取讲座的尝试次数, 在使用参数 '-r' 后生效.",
+    )
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
+    args = get_args()
+    C.logger.debug(f"{args=}")
+
     session, student_name = login_ehall(C.username, C.password)
     if student_name:
         C.logger.info(f"✨ {'***' if C.privacy_mode else student_name}, Welcome! ✨")
-        lectures = fetch_lecture_list(session=session)
-        render_table(lectures)
+        if args.repeat:
+            for i in range(args.times):
+                if i:  # sleep when i > 0
+                    time.sleep(args.interval)
+                lectures, available_lectures = fetch_lecture_list(session=session)
+                C.logger.info(
+                    f"发现 {len(lectures)} 个讲座, 其中 {available_lectures} 个可用. (第 {i + 1}/{args.times} 次检查)"
+                )
+                if available_lectures:
+                    call_browser()
+                    break
+        else:
+            lectures, available_lectures = fetch_lecture_list(session=session)
+            render_table(lectures)
